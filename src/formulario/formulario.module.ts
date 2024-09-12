297b08c3272baff6f304c1249372373d86b6093a
David Rios <david.r.v@hotmail.com>
2024-09-12 23:32:56 +0200

    agregando cambios

---
+++ src/formulario/formulario.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormularioComponent } from './formulario/formulario.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormularioComponent
+  ]
+})
+export class FormularioModule { }
---
+++ src/formulario/formulario/formulario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css',
+  standalone: true,
+})
+export class FormularioComponent {
+
+}
