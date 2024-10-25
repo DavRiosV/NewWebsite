bbe67e46d88a8094b74b98fede028739a3c460c6
David Rios <david.r.v@hotmail.com>
2024-10-25 18:06:33 -0300

    contacto component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormularioComponent } from '../formulario/formulario/formulario.component';
 import { HeroComponent } from '../hero/hero.component';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormularioComponent, HeroComponent],
+  imports: [RouterOutlet, FormularioComponent, HeroComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
